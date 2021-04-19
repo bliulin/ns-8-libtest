324ab7b36137dff69cc0d7772aa72f0682a8edbb
bliulin <bliulinx@gmail.com>
2021-04-19 18:32:45 +0300

    download file with http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptModule } from '@nativescript/angular'
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular'
+import { NativeScriptHttpClientModule } from "@nativescript/angular";
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [AppRoutingModule, NativeScriptModule, NativeScriptUISideDrawerModule],
+  imports: [AppRoutingModule, NativeScriptModule, NativeScriptUISideDrawerModule, NativeScriptHttpClientModule],
   declarations: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,50 @@
-import { Component, OnInit } from '@angular/core'
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer'
-import { Application } from '@nativescript/core'
-import { Canvas, CanvasView, createRect, Paint } from '@nativescript-community/ui-canvas';
-import { Color } from '@nativescript/core';
-import {exit} from 'nativescript-exit';
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { Application } from "@nativescript/core";
+import {
+  Canvas,
+  CanvasView,
+  createRect,
+  Paint
+} from "@nativescript-community/ui-canvas";
+import { Color } from "@nativescript/core";
+import { exit } from "nativescript-exit";
+
+import {DownloadManager} from 'nativescript-downloadmanager'
 
 import { registerElement } from "@nativescript/angular";
+import { HttpClient } from "@angular/common/http";
 registerElement("CanvasView", () => CanvasView);
 
 @Component({
-  selector: 'Home',
-  templateUrl: './home.component.html',
+  selector: "Home",
+  templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     // Use the component constructor to inject providers.
   }
 
   ngOnInit(): void {
     // Init your component properties here.
+
+    this.testDownload();
+  }
+
+  testDownload(): void {
+      this.httpClient.get("https://cachefly.cachefly.net/10mb.test").subscribe(
+        (result) => {
+          console.log('File downloaded: ' + result);
+        },
+      (err) => {
+        console.error('Failed to download file');
+        console.dir(err);
+      })
   }
 
   onDrawerButtonTap(): void {
-    const sideDrawer = <RadSideDrawer>Application.getRootView()
-    sideDrawer.showDrawer()
+    const sideDrawer = <RadSideDrawer>Application.getRootView();
+    sideDrawer.showDrawer();
   }
 
   onExitTapped(): void {
